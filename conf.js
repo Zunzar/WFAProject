97aca3cd1b92faa6123e3aed0e108377953b78ef
Zunzar C <zunzar.choudhary@trinet.com>
2018-03-08 14:26:41 -0600

    update the conf.js file

--- conf.js
+++ conf.js
@@ -1,20 +1,25 @@
 // An example configuration file.
 exports.config = {
-	//seleniumAddress: 'http://localhost:4444/wd/hub',
-  directConnect: true,
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	//chromeDriver: 'C:\Users\zchoudhary\eclipse-workspace\WFAProject1\node_modules\protractor\node_modules\webdriver-manager\selenium\chromedriver_2.35.exe',
+	geckodriver: 'C:\Users\zchoudhary\eclipse-workspace\WFAProject1\node_modules\protractor\node_modules\webdriver-manager\selenium\geckodriver-v0.19.1.exe',
+	seleniumServerJar: 'C:\Users\zchoudhary\eclipse-workspace\WFAProject1\node_modules\protractor\node_modules\webdriver-manager\selenium\selenium-server-standalone-3.9.1.jar',
+  //directConnect: true,
+  //firefox:true,
 
   // Capabilities to be passed to the webdriver instance.
   multiCapabilities: {
-    'browserName': 'Chrome',
+    'browserName': 'firefox',
+    'platfrom': 'ANY',
     
   },
 
   // Framework to use. Jasmine is recommended.
-  //framework: 'jasmine',
+  framework: 'jasmine',
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['Copy of AdminPage_spec.js'],
+  specs: ['Copy of ReportsHomePage_spec.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
